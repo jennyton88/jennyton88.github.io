44da248acae23e83e2474e535e6768a5e0ed02cc
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2024-08-29 18:34:55 -0400

    minor style modifications

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
     return (
-        <div>
+        <>
             <table>
-                <li><Link to={'/'}>Home</Link></li>
-                <li><Link to={'/portfolio'}>Portfolio</Link></li>
-                <li><Link to={'/projects'}>Projects</Link></li>
-                <li><Link to={'/devlogs'}>Devlogs</Link></li>
-                <li><Link to={'/artworks'}>Artworks</Link></li>
-                <li><Link to={'/contact'}>Contact</Link></li>
+                <tbody>
+                    <tr>
+                        <td>
+                            <li><Link to={'/'}>Home</Link></li>
+                            <li><Link to={'/portfolio'}>Portfolio</Link></li>
+                            <li><Link to={'/projects'}>Projects</Link></li>
+                            <li><Link to={'/devlogs'}>Devlogs</Link></li>
+                            <li><Link to={'/artworks'}>Artworks</Link></li>
+                            <li><Link to={'/contact'}>Contact</Link></li>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
-        </div>
+        </>
     )
 }
 
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,17 +1,17 @@
-import { useRouteError, useNavigate } from "react-router-dom";
+import { useRouteError, useNavigate } from 'react-router-dom';
 
 function Error() {
     const error = useRouteError();
     const navigate = useNavigate();
 
     return (
-        <div>
+        <>
             <p>Error</p>
             <p>{error.statusText || error.message}</p>
             <button  type="button" onClick={() => {navigate(-1);}}>
                 Return to previous page
             </button>
-        </div>
+        </>
     )
 }
 
