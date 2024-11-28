a1103d064c227f94567f193cfa0e81d99e238170
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2024-11-27 22:39:34 -0500

    add hr and link back to home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 function Footer() {
     return (
         <footer className="footer">
-            <p>Links</p> <Link to={"https://github.com/jennyton88"} className="horizontal-spacer">Github</Link><Link to={"https://jennydevs.itch.io/"}>Itch.io </Link>
+            <hr/>
+            <Link to={"/"} className="horizontal-spacer">Home</Link> <Link to={"https://github.com/jennyton88"} className="horizontal-spacer">Github</Link><Link to={"https://jennydevs.itch.io/"}>Itch.io </Link>
         </footer>
     )
 }
