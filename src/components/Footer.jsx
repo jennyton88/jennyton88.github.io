27db5ac931ab480f5abc59b3b219129977e40efe
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2024-11-30 11:47:16 -0500

    replace hr with proper border, remove classNames

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
     return (
-        <footer className="footer">
-            <hr/>
+        <footer>
             <Link to={"/"} className="horizontal-spacer">Home</Link> <Link to={"https://github.com/jennyton88"} className="horizontal-spacer">Github</Link><Link to={"https://jennydevs.itch.io/"}>Itch.io </Link>
         </footer>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 
 function Header({title}) {
     return (
-        <header className="header">
+        <header>
             <h1>{title}</h1>
-            <hr/>
         </header>
     )
 }
