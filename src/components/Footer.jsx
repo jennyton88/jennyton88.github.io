272fe470407debc0bb94d6442241688be4b485b5
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-03-28 17:37:20 -0400

    update footer with additional links and icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { FaHome, FaGithub, FaItchIo, FaMastodon } from 'react-icons/fa';
+import { FaBluesky } from 'react-icons/fa6';
+
 
 function Footer() {
     return (
         <footer>
-            <nav>
-                <Link to={"/"} className="horizontal-spacer">Home</Link>
-                <Link to={"https://github.com/jennyton88"} className="horizontal-spacer">Github</Link>
-                <Link to={"https://jennydevs.itch.io/"} className="horizontal-spacer">Itch </Link>
+            <nav className='footer-padding'>
+                <FaHome className='colored-logo'/>
+                <div className='left-hori-spacer'/>
+                <Link to={'/'}>Home</Link><div className='hori-spacer'/>
+                <FaGithub className='colored-logo'/>
+                <div className='left-hori-spacer'/>
+                <Link to={'https://github.com/jennyton88'}>Github</Link>
+                <div className='hori-spacer'/>
+                <FaItchIo className='colored-logo'/>
+                <div className='left-hori-spacer'/>
+                <Link to={'https://jennydevs.itch.io/'}>Itch.io</Link>
+                <div className='hori-spacer'/>
+                <FaMastodon className='colored-logo'/>
+                <div className='left-hori-spacer'/>
+                <Link to={'https://mastodon.gamedev.place/@jennydevs'}>Mastodon</Link>
+                <div className='hori-spacer'/>
+                <FaBluesky className='colored-logo'/>
+                <div className='left-hori-spacer'/>
+                <Link to={'https://bsky.app/profile/jennydevs.bsky.social'}>Bluesky</Link>
             </nav>
         </footer>
-    )
+    );
 }
 
 export default Footer;
