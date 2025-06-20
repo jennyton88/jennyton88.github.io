eb225ad5a3dac731fbbcb5fd18e42a11dfb834de
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-06-20 10:27:40 -0400

    update projects headers, content

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,6 +9,8 @@ function Projects() {
             <header><h1 className='front-name'>Projects</h1></header>
             <Topbar/>
             <div className='content'>
+                <h1>Recent projects</h1>
+                <p>Here is the list of projects I have worked on.</p>
                 <ProjectList/>
             </div>
             <Footer />
