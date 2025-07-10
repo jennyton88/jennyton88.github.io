dd9193726bf4b8a207c41278c57237bd9e464ab1
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-07-10 12:29:59 -0400

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import Topbar from '../components/Topbar.jsx';
 import ProjectList from '../components/ProjectList.jsx';
 import Footer from '../components/Footer.jsx';
@@ -11,11 +13,18 @@ function Home() {
             <div className='content'>
                 <h1>About Me</h1>
                 <hr/>
-                <p>Hi, I like to create software!</p>
-                <p>I'm always on the lookout for subjects that interest me. I like solving various problems and learning about interesting things!</p>
-                <br/>
+                <p>Hi, I'm Jenny, I'm looking to make interesting things.</p>
                 <h2>What am I currently working on?</h2>
-                <p>I'm working on improving <a href="https://github.com/jennyton88/devlogs_maker">Devlogs Maker</a>, which you can find on my Github.</p>
+                <p>I'm working on improving <Link to="https://github.com/jennyton88/devlogs_maker">Devlogs Maker</Link>, which you can find on my Github. I'm also working on some games, and additional software in the background. You may find some updates about them occasionally on my social media.</p>
+                <h2>Interests</h2>
+                <p style={{paddingLeft:"15px"}}>
+                    <li>Printmaking</li>
+                    <li>Papermaking</li>
+                    <li>Digital Drawing</li>
+                    <li>Making Games</li>
+                    <li>3D Modeling</li>
+                    <li>Video Editing</li>
+                </p>
                 <br/>
                 <h1>Recent Projects</h1>
                 <hr/>
