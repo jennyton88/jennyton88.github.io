35094d4d4d2fa1ec5c3541cd96ec8e3fe2ef053e
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-07-10 13:31:19 -0400

    update styles for topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 function Topbar() {
     return (
         <nav className='topbar'>
-            <li className='hori-spacer'><Link to={'/'}>Home</Link></li>
-            <li className='hori-spacer'><Link to={'/projects'}>Projects</Link></li>
-            <li className='hori-spacer'><Link to={'/devlogs'}>Devlogs</Link></li>
-            <li className='hori-spacer'><Link to={'/contact'}>Contact</Link></li>
+            <li className='hori-top-spacer'><Link to={'/'}>Home</Link></li>
+            <li className='hori-top-spacer'><Link to={'/projects'}>Projects</Link></li>
+            <li className='hori-top-spacer'><Link to={'/devlogs'}>Devlogs</Link></li>
+            <li className='hori-top-spacer'><Link to={'/contact'}>Contact</Link></li>
         </nav>
     );
 }
