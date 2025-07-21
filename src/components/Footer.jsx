365bbfa62ea61fb33401b1edfd307242f6130e06
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-07-21 13:26:03 -0400

    Move links from footer to homepage, add new link, update text, update css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,7 @@
-import { Link } from 'react-router-dom';
-import { FaHome, FaGithub, FaItchIo, FaMastodon } from 'react-icons/fa';
-import { FaBluesky } from 'react-icons/fa6';
-
-
 function Footer() {
     return (
         <footer>
-            <nav>
-                <li className='hori-spacer'>
-                    <Link to={'/'}><FaHome className='colored-logo left-hori-spacer'/>Home</Link>
-                </li>
-                <li className='hori-spacer'>
-                    <Link to={'https://github.com/jennyton88'}><FaGithub className='colored-logo left-hori-spacer'/>Github</Link>
-                </li>
-                <li className='hori-spacer'>
-                    <Link to={'https://jennydevs.itch.io/'}><FaItchIo className='colored-logo left-hori-spacer'/>Itch.io</Link>
-                </li>
-                <li className='hori-spacer'>
-                    <Link to={'https://mastodon.gamedev.place/@jennydevs'}><FaMastodon className='colored-logo left-hori-spacer'/>Mastodon</Link>
-                </li>
-                <li className='hori-spacer'>
-                    <Link to={'https://bsky.app/profile/jennydevs.bsky.social'}><FaBluesky className='colored-logo left-hori-spacer'/>Bluesky</Link>
-                </li>
-            </nav>
-            <div><small>© Jenny Ton</small></div>
+            <small>© Jenny Ton 2023-2025</small>
         </footer>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 
+import { FaGithub, FaItchIo, FaMastodon, FaBluesky, FaYoutube } from 'react-icons/fa6';
+
 import Topbar from '../components/Topbar.jsx';
 import ProjectList from '../components/ProjectList.jsx';
 import Footer from '../components/Footer.jsx';
@@ -11,21 +13,29 @@ function Home() {
             <header><h1 className='front-name'>Jenny Ton</h1></header>
             <Topbar />
             <div className='content'>
-                <h1>About Me</h1>
+                <h1>Hi, I'm Jenny!</h1>
                 <hr/>
-                <p>Hi, I'm Jenny, I'm looking to make interesting things.</p>
-                <h2>What am I currently working on?</h2>
+                <h2>What am I working on?</h2>
                 <p>I'm working on improving <Link to="https://github.com/jennyton88/devlogs_maker">Devlogs Maker</Link>, which you can find on my Github. I'm also working on some games, and additional software in the background. You may find some updates about them occasionally on my social media.</p>
-                <h2>Interests</h2>
+                <h2>My Links</h2>
+                <nav style={{listStyle: 'none'}}>
+                    <li><Link to={'https://github.com/jennyton88'}><FaGithub className='logo-link'/>Github</Link></li>
+                    <li><Link to={'https://jennydevs.itch.io/'}><FaItchIo className='logo-link'/>Itch.io</Link></li>
+                    <li><Link to={'https://bsky.app/profile/jennydevs.bsky.social'}><FaBluesky className='logo-link'/>Bluesky</Link></li>
+                    <li><Link to={'https://mastodon.gamedev.place/@jennydevs'}><FaMastodon className='logo-link'/>Mastodon</Link></li>
+                    <li><Link to={'https://www.youtube.com/@jennydevs'}><FaYoutube className='logo-link'/>YouTube</Link></li>
+                </nav>
+                <p>I compiled my links onto <Link to={'https://linktr.ee/jennydevs'}>Linktree</Link> so you can bookmark them!</p>
+                <h2>What subjects am I exploring?</h2>
                 <p style={{paddingLeft:"15px"}}>
+                    <li>Making Games</li>
                     <li>Printmaking</li>
                     <li>Papermaking</li>
-                    <li>Digital Drawing</li>
-                    <li>Making Games</li>
+                    <li>Stamps</li>
+                    <li>Digital Art</li>
                     <li>3D Modeling</li>
                     <li>Video Editing</li>
                 </p>
-                <br/>
                 <h1>Recent Projects</h1>
                 <hr/>
                 <ProjectList/>
