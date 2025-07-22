91511fbbdeea58d6967bee76454978556d26952c
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-07-22 11:45:49 -0400

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,31 +15,40 @@ function Home() {
             <Topbar />
             <div className='content'>
                 <h1>Hi, I'm Jenny!</h1>
+                <img src="/images/border.png" width="100%" height="100%" className='img-border'/>
+                <h2>I'm currently working on...</h2>
+                <p>Improving <Link to="https://github.com/jennyton88/devlogs_maker">Devlogs Maker</Link>, which you can find on my Github. I'm also working on some games, and additional software in the background. You may find updates about them on my social media.</p>
                 <hr/>
-                <h2>What am I working on?</h2>
-                <p>I'm working on improving <Link to="https://github.com/jennyton88/devlogs_maker">Devlogs Maker</Link>, which you can find on my Github. I'm also working on some games, and additional software in the background. You may find some updates about them occasionally on my social media.</p>
-                <h2>My Links</h2>
-                <nav style={{listStyle: 'none'}}>
-                    <li><Link to={'https://github.com/jennyton88'}><FaGithub className='logo-link'/>Github</Link></li>
-                    <li><Link to={'https://jennydevs.itch.io/'}><FaItchIo className='logo-link'/>Itch.io</Link></li>
-                    <li><Link to={'https://bsky.app/profile/jennydevs.bsky.social'}><FaBluesky className='logo-link'/>Bluesky</Link></li>
-                    <li><Link to={'https://mastodon.gamedev.place/@jennydevs'}><FaMastodon className='logo-link'/>Mastodon</Link></li>
-                    <li><Link to={'https://www.youtube.com/@jennydevs'}><FaYoutube className='logo-link'/>YouTube</Link></li>
-                </nav>
-                <p>I compiled my links onto <Link to={'https://linktr.ee/jennydevs'}>Linktree</Link> so you can bookmark them!</p>
-                <h2>What subjects am I exploring?</h2>
-                <p style={{paddingLeft:"15px"}}>
-                    <li>Making Games</li>
-                    <li>Printmaking</li>
-                    <li>Papermaking</li>
-                    <li>Stamps</li>
-                    <li>Digital Art</li>
-                    <li>3D Modeling</li>
-                    <li>Video Editing</li>
-                </p>
+                <div className='row'>
+                    <div style={{display:'block', width:'50%'}}>
+                        <h2 style={{display:'block'}}>Exploring subjects like...</h2>
+                        <p style={{paddingLeft:"15px"}}>
+                            <li>Making Games</li>
+                            <li>Printmaking</li>
+                            <li>Papermaking</li>
+                            <li>Stamps</li>
+                            <li>Digital Art</li>
+                            <li>3D Modeling</li>
+                            <li>Video Editing</li>
+                        </p>
+                    </div>
+                    <div style={{display:'block', width:'50%'}}>
+                        <h2>My links below!</h2>
+                        <nav style={{listStyle: 'none'}}>
+                            <li><Link to={'https://linktr.ee/jennydevs'}>Linktree</Link></li>
+                            <li><Link to={'https://github.com/jennyton88'}><FaGithub className='logo-link'/>Github</Link></li>
+                            <li><Link to={'https://jennydevs.itch.io/'}><FaItchIo className='logo-link'/>Itch.io</Link></li>
+                            <li><Link to={'https://bsky.app/profile/jennydevs.bsky.social'}><FaBluesky className='logo-link'/>Bluesky</Link></li>
+                            <li><Link to={'https://mastodon.gamedev.place/@jennydevs'}><FaMastodon className='logo-link'/>Mastodon</Link></li>
+                            <li><Link to={'https://www.youtube.com/@jennydevs'}><FaYoutube className='logo-link'/>YouTube</Link></li>
+                        </nav>
+                    </div>
+                </div>
+                <hr/>
                 <h1>Recent Projects</h1>
-                <hr/>
+                <img src="/images/border.png" width="100%" height="100%" className='img-border'/>
                 <ProjectList/>
+                <img src="/images/border.png" width="100%" height="100%" className='img-border'/>
             </div>
             <Footer />
         </div>
